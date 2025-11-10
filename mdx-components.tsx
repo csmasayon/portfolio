0180fb26d49ceee33dd0c067560f940ed9c5c603
csmasayon <70651638+csmasayon@users.noreply.github.com>
2025-11-10 22:44:38 +0800

    Justify MDX paragraphs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from 'mdx/types'
 import Link from 'next/link';
  
 const components: MDXComponents = {
+  p: ({ children }) => <p className="text-justify">{children}</p>,
 }
  
 export function useMDXComponents(): MDXComponents {
