484206afc51f01086d93993398635ce0aabdc9b6
csmasayon <70651638+csmasayon@users.noreply.github.com>
2025-11-12 06:55:07 +0800

    Automate footer year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,14 @@
 import { Github, Linkedin, Mail } from "lucide-react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="border-t max-w-6xl mt-4 sm:mt-8 sm:mb-8 lg:mx-auto sm:mx-auto space-y-8 mx-4">
       <div className="flex flex-col sm:flex-row justify-between items-center gap-4 mt-4 mb-8">
         <div className="text-center sm:text-left">
           <div>
-            © 2025 Christian Ace Masayon. All rights reserved.
+            © {currentYear} Christian Ace Masayon. All rights reserved.
           </div>
         </div>
         <div className="flex gap-2">
